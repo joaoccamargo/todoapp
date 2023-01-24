b5c0218ac5364c9a7f14e7d84fe06f63e34f876c
João Camargo <jclaudio00_12@hotmail.com>
2023-01-24 10:00:01 -0300

    added save data and construct task

--- app/src/main/java/controller/TaskController.java
+++ app/src/main/java/controller/TaskController.java
@@ -27,9 +27,11 @@ public class TaskController {
             statement.setBoolean(4, task.isIsCompleted());
             statement.setString(5, task.getNotes());
             statement.setDate(6, new Date(task.getDeadline().getTime()));
-            
-        } catch (SQLException e) {
-            throw new SQLException("Erro ao salvar a tarefa");
+            statement.setDate(7, new Date(task.getCreatedAt().getTime()));
+            statement.setDate(8, new Date(task.getUpdatedAt().getTime()));
+            statement.execute();
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao salvar a tarefa" + ex.getMessage(), ex);
         } finally {
             ConnectionFactory.closeConnection(conn);
         }
